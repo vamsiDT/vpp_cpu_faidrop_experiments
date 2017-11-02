0aa94914a6ab8626968d8e12c5fe2cf309e3bcf3
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-11-02 17:20:24 +0100

    cpu experiments

---
+++ experiment.sh
@@ -0,0 +1,44 @@
+WORKDIR="/home/vk/fairdrop_cpu_experiments/cpu_weights_ip4"
+SCRIPTS="/home/vk/scripts_cpu"
+BW=0.1
+
+sudo killall vpp_main
+sudo killall pktgen
+
+#CHECK DPDK-SETUP BEFORE RUNNING THE EXPERIMENT#
+
+######################WIthout cpu turbo boost#####################
+
+BW=0.1
+
+echo "disabling turbo boost"
+echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+until  [ $(echo $BW | awk -F "." '{print $1}') -gt 0 -a $(echo $BW | awk -F "." '{print $2}') -gt 0 ]
+do
+    echo -e "\n\n\nPerforming experiment for Bandwidth limit $BW factor of cpu 2.6Ghz\n\n\n"
+    sleep 3
+    sudo rm /dev/hugepages/*
+    echo 60 | sudo tee /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
+    sudo -E $SCRIPTS/change-bw-limit.sh 1 $BW #check this line
+	cd $VPP_ROOT
+    make build-release
+    echo -e "\n\n\nStarting VPP in l3 forwarding mode with $NAMELC1P0 (Receiving interface) and $NAMELC1P1 (Transmitting Interface)\n\n\n"
+    sleep 3
+    $SCRIPTS/vpp_l3.sh &
+    sleep 20
+    sudo -E $SCRIPTS/ctl.sh
+    echo -e "\n\n\nStarting Dpdk-Pktgen with $LC0P0 (Transmitting Interface) and $LC0P1 (Receiving Interface)\n\n\n"
+    #screen -L
+    $SCRIPTS/pktgen_capture.sh
+    sudo killall vpp_main
+    sudo killall pktgen
+    cp /tmp/show $WORKDIR/$BW.dat
+    BW=$(python -c "print($BW+0.1)")
+done
+
+echo "################################"
+echo -e "\n"
+echo "SUCCESSFULLY FINISHED EXPERIMENT"
+echo -e "\n"
+echo "################################"
