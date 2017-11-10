c82fef6e22957bf1bbef17237bcb1f09a467ffd8
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-11-10 14:22:04 +0100

    update

--- experiment.sh
+++ experiment.sh
@@ -9,7 +9,7 @@ sudo killall pktgen
 
 ######################WIthout cpu turbo boost#####################
 
-BW=0.92
+BW=0.91
 
 echo "disabling turbo boost"
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
