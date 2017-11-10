704218afee36280031abc693b0932160443f0869
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-11-10 16:43:42 +0100

    update

--- plots/experiment.sh
+++ plots/experiment.sh
@@ -1,7 +1,7 @@
 WORKDIR="/home/vk/vpp_cpu_faidrop_experiments"
-BW=0.91
+BW=0.90
 
-until  [ $(echo $BW | awk -F "." '{print $1}') -ge 0 -a $(echo $BW | awk -F "." '{print $2}') -eq 92  ]
+until  [ $(echo $BW | awk -F "." '{print $1}') -gt 0 -a $(echo $BW | awk -F "." '{print $2}') -eq 1  ]
 do
 	cat $WORKDIR/"$BW"_run.dat | tail -n 13 > $WORKDIR/plots/"$BW"_run.dat
     BW=$(python -c "print($BW+0.01)")
