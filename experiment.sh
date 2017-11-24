005e9444d1f0d67dde8c4ec63e4ca0d55ae40d0a
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-11-24 14:05:40 +0100

    update

--- experiment.sh
+++ experiment.sh
@@ -4,6 +4,9 @@ FLOW="/home/vk/FLOW_MONITOR/DPDK-FlowCount"
 sudo killall FlowCount
 sudo killall vpp_main
 sudo killall pktgen
+cd $EXP
+sudo rm ./*.dat
+sudo rm flowvqueue*
 cd $FLOW
 sudo -E ./build/FlowCount -l 38,39,40 --file-prefix flow --socket-mem 4096,4096 -b $LC1P1 -b $LC1P0 -b $LC0P0 >$EXP/flow_monitor.dat &
 sleep 30
@@ -14,6 +17,3 @@ sudo -E $SCRIPTS/pktgen_capture.sh
 cp /tmp/show $EXP/showrun.dat
 cp /tmp/data $EXP/showint.dat
 
-#post processing
-cd $EXP
-cat flow_monitor.dat | tail -l 13 > flow_throughput.dat
