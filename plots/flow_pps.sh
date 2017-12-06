6c0a58a1bae58633f2cbc7680535aebf44b4a53a
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-12-06 02:45:31 +0100

    update

--- plots/flow_pps.sh
+++
@@ -1,2 +0,0 @@
-VPP_GIT=$HOME/VPP
-cat $VPP_GIT/vpp_cpu_faidrop_experiments/flow_monitor.dat | tail --lines 31 | head --lines 21 | awk '{print $7"\t"$14}' | awk -F "\t|:" '{if ($1>1)print $1"\t"$3}' > flow_pps.dat
